d2cf93eb5e631a2f371477dba8af16dd0072414d
Black Mirror <blackmirror1980@gmail.com>
2019-02-04 14:08:56 +0100

    fix polyfills object.prototype.merge

--- src/Core/Polyfills.js
+++ src/Core/Polyfills.js
@@ -2,7 +2,7 @@
 /* eslint-disable prefer-rest-params */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-bitwise */
-if(!String.prototype.toCamelCase) {
+if (!String.prototype.toCamelCase) {
   Object.defineProperty(String.prototype, 'toCamelCase', {
     enumerable: false,
     configurable: true,
@@ -47,14 +47,17 @@ if (!Object.assign) {
   });
 }
 
-if(!Object.prototype.merge) {
+if (!Object.prototype.merge) {
   Object.defineProperty(Object.prototype, 'merge', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
     value(...objs) {
       const mergeObj = [this, ...objs].reduce((acc, obj) => Object.keys(obj).reduce((a, k) => {
-        if(acc.hasOwnProperty(k)) {
-          if(acc[k] instanceof Array) {
+        if (acc.hasOwnProperty(k)) {
+          if (acc[k] instanceof Array) {
             acc[k] = [].concat(acc[k], obj[k]);
-          } else if(acc[k] instanceof Object && Object.keys(acc[k]).length > 0 && obj[k] instanceof Object && Object.keys(obj[k]).length > 0) {
+          } else if (acc[k] instanceof Object && Object.keys(acc[k]).length > 0 && obj[k] instanceof Object && Object.keys(obj[k]).length > 0) {
             acc[k].merge(obj[k]);
           } else {
             acc[k] = obj[k];
@@ -71,7 +74,7 @@ if(!Object.prototype.merge) {
   });
 }
 
-if(!Array.prototype.map) {
+if (!Array.prototype.map) {
   Object.defineProperty(Array.prototype, 'map', {
     value(arr, iterator) {
       const result = [];
