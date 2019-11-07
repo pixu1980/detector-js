7e573d077922ad70154ce74e050671e67ed22202
Black Mirror <blackmirror1980@gmail.com>
2019-11-07 14:42:10 +0100

    hotfix module export

--- src/index.es.js
+++ src/index.es.js
@@ -2,4 +2,5 @@ import './Core/Polyfills';
 
 import Detector from './Detector';
 
+// module export
 export default Detector;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './Core/Polyfills';
 import Detector from './Detector';
 
 if(typeof module !== 'undefined') {
+  // module export
   module.exports = Detector;
 }
